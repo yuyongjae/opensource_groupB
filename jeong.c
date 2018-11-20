9802363b846e3531d543d5fe7aad0670c5959fb8
sehyeon jeong <shyun0303@gmail.com>
2018-11-20 15:35:14 +0900

    add "reference"

--- jeong.c
+++ jeong.c
@@ -3,67 +3,44 @@
 #include <time.h>
 #include <string.h>
 #pragma warning (disable:4996)//scanf함수는 visual studio에서 에러메세지를 동반하므로 본 메시지 추가.
-
-#define computer_num 4 // 컴퓨터랜덤 자리숫자 입력 
+ #define computer_num 4 // 컴퓨터랜덤 자리숫자 입력 
 #define user_num computer_num //유저랜덤자리숫자 
 #define number_counter (computer_num*2)+2
-
-
-
-int random_number(int random_number[]); //컴퓨터 랜덤함수
+ int random_number(int random_number[]); //컴퓨터 랜덤함수
 int play_game_user(int number[]); //숫자입력 
-
-void number_result(int computer[], int player[], int strike_count[], int ball_count[]);
-
-/*-------------------------------------------------------------------------------------------------------------*/
-
-int main()
+ void number_result(int computer[], int player[], int strike_count[], int ball_count[]);
+ /*-------------------------------------------------------------------------------------------------------------*/
+ int main()
 {
-
-	int computer[computer_num]; //컴퓨터랜덤숫자
+ 	int computer[computer_num]; //컴퓨터랜덤숫자
 	random_number(computer); //함수로부터 랜덤숫자 가져옴
 	int player[user_num]; // 숫자입력 초기화 
-
-	printf("%d %d %d %d\n", computer[0], computer[1], computer[2], computer[3]);
-
-	int strike_count[number_counter] = { 0 }, ball_count[number_counter] = { 0 };
-
-	while (1)
+ 	printf("%d %d %d %d\n", computer[0], computer[1], computer[2], computer[3]);
+ 	int strike_count[number_counter] = { 0 }, ball_count[number_counter] = { 0 };
+ 	while (1)
 	{
 		play_game_user(player);
 		number_result(computer, player, strike_count, ball_count);
 	}
-
-
-	system("pause");
+ 	system("pause");
 }
-
-/*-------------------------------------------------------------------------------------------------------------*/
-
-
-int random_number(int random_number[])
+ /*-------------------------------------------------------------------------------------------------------------*/
+ int random_number(int random_number[])
 {
-
-	srand(time(NULL));
-
-	int i;
-
-re_number: //goto문
-
-	for (i = 0; i <= computer_num; i++) //컴퓨터 지정자리만큼 숫자랜덤지정
+ 	srand(time(NULL));
+ 	int i;
+ re_number: //goto문
+ 	for (i = 0; i <= computer_num; i++) //컴퓨터 지정자리만큼 숫자랜덤지정
 	{
 		random_number[i] = rand() % 10;
 	}
-
-	int a = 0;
-
-	for (i; i <= computer_num; i++) //나머지 (컴퓨터지정숫자-1개) 만큼 배열 0부터 복사 
+ 	int a = 0;
+ 	for (i; i <= computer_num; i++) //나머지 (컴퓨터지정숫자-1개) 만큼 배열 0부터 복사 
 	{
 		random_number[i] = random_number[a];
 		a++;
 	}
-
-	for (i = 0; i < computer_num; i++)
+ 	for (i = 0; i < computer_num; i++)
 	{
 		if (i == computer_num) //배열 목표지점도착시 반복문 종료 
 		{
@@ -74,33 +51,23 @@ re_number: //goto문
 			goto re_number;
 		}
 	}
-
-	return *random_number;
+ 	return *random_number;
 }
-
-int play_game_user(int number[])
+ int play_game_user(int number[])
 {
 	static count_main = 1;
-
-	printf("%d번째 기회 !!!!\n", count_main);
+ 	printf("%d번째 기회 !!!!\n", count_main);
 	printf("숫자를 입력해주세요 >> ");
 	fflush(stdout);
 	scanf("%d %d %d %d", &number[0], &number[1], &number[2], &number[3]);
 	count_main++;
 	return *number;
-
-}
-
-
-void number_result(int computer[], int player[], int strike_count[], int ball_count[])
+ }
+ void number_result(int computer[], int player[], int strike_count[], int ball_count[])
 {
-
-
-	static count;
+ 	static count;
 	int j, k, m;
-
-
-	for (j = 0; j < user_num; j++)
+ 	for (j = 0; j < user_num; j++)
 	{
 		for (k = 0; k < user_num; k++)
 		{
@@ -125,29 +92,20 @@ void number_result(int computer[], int player[], int strike_count[], int ball_co
 	printf("\n\n");
 	printf("strike = %d , ball = %d", strike_count[count], ball_count[count]);
 	printf("\n\n\n");
-
-
-	if (strike_count[count] == 4)
+ 	if (strike_count[count] == 4)
 	{
 		printf("게임에 승리하셧습니다.");
 		printf("\n");
 		system("pause");
 		exit(0);
-
-	}
-
-	count++;
-
-	if (count >= 10)
+ 	}
+ 	count++;
+ 	if (count >= 10)
 	{
 		printf("게임에 패배하셨습니다.");
 		printf("\n");
 		system("pause");
 		exit(0);
-
-	}
+ 	}
 }
-
-
-
-
+ ////////////////////////함수 기능//////////////////////////////////////////////////
